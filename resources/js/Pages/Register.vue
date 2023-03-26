99496a182f417160084f066bd64d396fa24aa55f
Saroar Hossain <limonhossain57@gmail.com>
2023-03-26 17:07:57 +0600

    Fixed: menu links and added items to register form

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -5,25 +5,48 @@
             <p class="text-center text-gray-500 mb-8">Please enter all the details and register your account</p>
             <form>
                 <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="email">
-                        Email
-                    </label>
+                    <label class="block text-gray-700 font-bold mb-2" for="name">Name</label>
+                    <input
+                        class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="name"
+                        type="text"
+                        placeholder="Enter full name"
+                    >
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 font-bold mb-2" for="email">Email</label>
                     <input
                         class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="email"
                         type="email"
-                        placeholder="Email"
+                        placeholder="Enter your email"
+                    >
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 font-bold mb-2" for="Number">Phone Number</label>
+                    <input
+                        class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="number"
+                        type="text"
+                        placeholder="Enter phone number"
                     >
                 </div>
                 <div class="mb-6">
-                    <label class="block text-gray-700 font-bold mb-2" for="password">
-                        Password
-                    </label>
+                    <label class="block text-gray-700 font-bold mb-2" for="password">Password</label>
                     <input
                         class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="password"
                         type="password"
-                        placeholder="Password"
+                        placeholder="Enter password"
+                    >
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 font-bold mb-2" for="repeat_password">Repeat Password</label>
+                    <input
+                        class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="repeat_password"
+                        type="password"
+                        placeholder="Repeat password"
                     >
                 </div>
                 <button class="bg-orange-500 hover:bg-orange-600 text-white px-8 py-2 rounded-md" type="button">Login</button>
