8c8cd7993b707f25e14cc0ee098110dd1766930c
Saroar Hossain <limonhossain57@gmail.com>
2023-04-04 21:28:20 +0600

    Updated copyright

--- resources/js/Components/TemplateParts/Footer.vue
+++ resources/js/Components/TemplateParts/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="bg-slate-800 py-5">
         <div class="container mx-auto">
             <div class="text-white text-center">
-                Copyright © 2022
+                Copyright © 2023
             </div>
         </div>
     </footer>
