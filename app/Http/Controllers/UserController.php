79a3c573db0a2e040fde6b2943c60af62841c2f6
Saroar Hossain <limonhossain57@gmail.com>
2023-04-07 13:13:32 +0600

    Added: Login API route for login user  using username and password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -34,41 +35,45 @@ class UserController extends Controller
                 'user' => $user
             ]
         ]);
-
-        //dd($validatedData);
-
-        // $validatedData['password'] = Hash::make($validatedData['password']);
-
-        // $user = User::create($validatedData);
-
-        // $token = $user->createToken('auth_token')->plainTextToken;
-
-        // return response()->json([
-        //     'access_token' => $token,
-        //     'token_type' => 'Bearer',
-        // ]);
     }
 
     public function login(Request $request)
     {
-        $validatedData = $request->validate([
-            'email' => 'email|required|max:255',
-            'password' => 'required|min:8',
+        // I could use the a separate validation method but I wanted to keep it simple because I learning new things by that project. So let's test another way to do it 🔥.
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|max:50',
+            'password' => 'required',
         ]);
-
-        $user = User::where('email', $validatedData['email'])->first();
-
-        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+ 
+        if ($validator->fails()) {
             return response()->json([
-                'message' => 'Invalid credentials',
+                'success'   => false,
+                'message'   => 'Validation errors',
+                'data'      => $validator->errors()
+            ]);
+        }
+ 
+        $validated = $validator->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Your credentials are incorrect',
             ], 401);
         }
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
+            'success'   => true,
+            'message'   => 'Login successful',
+            'data'      => [
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ]
         ]);
     }
 }
